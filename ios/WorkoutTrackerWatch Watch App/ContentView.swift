030a1491fc078b44a368ca11a2aa6c23d4136e5d
Artem Pasyechnyk <apasyechnyk@figma.com>
2025-12-02 22:15:20 -0800

    add watch app scaffold

---
+++ ios/WorkoutTrackerWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutTrackerWatch Watch App
+//
+//  Created by Artem Pasyechnyk on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/WorkoutTrackerWatch Watch App/WorkoutTrackerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerWatchApp.swift
+//  WorkoutTrackerWatch Watch App
+//
+//  Created by Artem Pasyechnyk on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
